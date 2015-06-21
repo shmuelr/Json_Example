3b6d98d24cb4273a7430d737084edcfdc73e8045
shmuelr <sdrosansky@gmail.com>
2015-06-21 14:41:38 -0400

    Recyclerview stuff

--- app/src/main/java/com/orbitdesign/jsonexample/adapters/DefinitionAdapter.java
+++ app/src/main/java/com/orbitdesign/jsonexample/adapters/DefinitionAdapter.java
@@ -1,9 +1,13 @@
 package com.orbitdesign.jsonexample.adapters;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.graphics.ColorUtils;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.orbitdesign.jsonexample.R;
@@ -17,15 +21,18 @@ import java.util.List;
  */
 public class DefinitionAdapter extends RecyclerView.Adapter<DefinitionAdapter.DefinitionViewHolder> {
 
+    private static final int purpleColor = Color.parseColor("#ff00ff");
 
     protected static class DefinitionViewHolder extends RecyclerView.ViewHolder{
 
         public TextView textViewText, textViewAttribution;
+        private LinearLayout backgroundLayout;
 
         public DefinitionViewHolder(View itemView) {
             super(itemView);
             textViewText = (TextView)itemView.findViewById(R.id.textViewText);
             textViewAttribution = (TextView)itemView.findViewById(R.id.textViewAttribution);
+            backgroundLayout = (LinearLayout)itemView.findViewById(R.id.item_background);
         }
 
     }
@@ -50,11 +57,21 @@ public class DefinitionAdapter extends RecyclerView.Adapter<DefinitionAdapter.De
 
     @Override
     public void onBindViewHolder(DefinitionViewHolder definitionViewHolder, int position) {
+        int pos = position % 3;
 
         Definition definition = definitionList.get(position);
 
         definitionViewHolder.textViewText.setText(definition.getText());
         definitionViewHolder.textViewAttribution.setText(definition.getAttribution());
+
+        if(pos == 1){
+            definitionViewHolder.backgroundLayout.setBackgroundColor(Color.RED);
+        }else if (pos == 2){
+            definitionViewHolder.backgroundLayout.setBackgroundColor(Color.WHITE);
+        }else {
+            definitionViewHolder.backgroundLayout.setBackgroundColor(purpleColor);
+        }
+
     }
 
     @Override
